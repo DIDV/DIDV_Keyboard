94aee2dbfb1981e2b9d821c885ce7ed465476914
Vitor Hugo <pi@raspberrypi.(none)>
2015-02-28 11:39:07 -0300

    Observa dois botões, porem sem a declaração de objetos separados para cada

--- keyboard.rb
+++ keyboard.rb
@@ -8,33 +8,24 @@ class Keyboard
 
   def initialize
     @letter = 0x0
-    @button1 = Pin.new(:pin => 02, :direction => :in, invert: true)
+    #@button1 = Pin.new(:pin => 02, :direction => :in, invert: true)
     @led = Pin.new(:pin => 03, :direction=> :out)
   end
 
   def wait_keyboard(times=nil)
-
-      puts "Inicio"
-      #unless times
-      loop do
-        @button1.wait_for_change
-        if @button1.value == 1
-          puts "HIGH :D"
-        else
-          puts "low :("
-        end
+    watch :pin => 02 do
+      #puts "Pin 02 changed from #{last_value} to #{value}"
+      if value == 1
+        puts "click 1"
       end
-      # else
-      #   times.times do
-      #     @button1.wait_for_change
-      #     if @button1.value == 1
-      #       puts "HIGH :D"
-      #     else
-      #       puts "low :("
-      #     end
-      #   end
-      # end
-    puts "Fim"
+    end
+
+    watch :pin => 03 do
+      if value ==1
+        puts "click 2"
+      end
+    end
+    wait
   end
 end
 
