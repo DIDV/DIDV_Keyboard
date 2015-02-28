1de133fb769cf5b990b03c1d2bd33035afc4533f
Vitor Hugo <pi@raspberrypi.(none)>
2015-02-28 13:33:34 -0300

    Multiplos pinos podem ser observado agora

--- keyboard.rb
+++ keyboard.rb
@@ -1,34 +1,40 @@
 require 'pi_piper'
 #require 'pry'
 include PiPiper
+#require 'eventmachine'
 
 class Keyboard
 
-  attr_accessor :letter,:button1,:led
+  attr_accessor :letter,:button1, :button2,:led
 
   def initialize
     @letter = 0x0
-    #@button1 = Pin.new(:pin => 02, :direction => :in, invert: true)
-    @led = Pin.new(:pin => 03, :direction=> :out)
+    @button1 = Pin.new(:pin => 02, :direction => :in, invert: true)
+    @button2 = Pin.new(:pin => 03, :direction => :in, invert: true)
+    #@led = Pin.new(:pin => 0, :direction=> :out)
   end
 
-  def wait_keyboard(times=nil)
-    watch :pin => 02 do
-      #puts "Pin 02 changed from #{last_value} to #{value}"
-      if value == 1
-        puts "click 1"
+  def listen_keyboard(button_object)
+    puts 'Inicializando Botão'
+    thread = Thread.new do
+      loop do
+        button_object.wait_for_change
+          if button_object.value == 1
+            puts "HIGH :D #{button_object.pin}"
+          end
       end
     end
+    thread.abort_on_exception = true
+    thread
+  end
 
-    watch :pin => 03 do
-      if value ==1
-        puts "click 2"
-      end
-    end
+  def wait_keyboard
+    listen_keyboard(@button1)
+    listen_keyboard(@button2)
     wait
   end
-end
 
+end
 key = Keyboard.new
 key.wait_keyboard
 
