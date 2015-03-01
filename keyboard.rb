a6409af9b00ca426148b70cd8f57213f28b9ae55
Vitor Hugo <pi@raspberrypi.(none)>
2015-03-01 16:42:03 -0300

    Deboucing Simples

--- keyboard.rb
+++ keyboard.rb
@@ -1,27 +1,33 @@
 require 'pi_piper'
-#require 'pry'
 include PiPiper
+#require 'pry'
 #require 'eventmachine'
 
 class Keyboard
 
-  attr_accessor :letter,:button1, :button2,:led
+  attr_accessor :letter,:button1,:button2,:button3,:led,:timer
 
   def initialize
     @letter = 0x0
     @button1 = Pin.new(:pin => 02, :direction => :in, invert: true)
     @button2 = Pin.new(:pin => 03, :direction => :in, invert: true)
+    @button3 = Pin.new(:pin => 04, :direction => :in, invert: true)
+    #@timer = Timers::Group.new
     #@led = Pin.new(:pin => 0, :direction=> :out)
   end
 
   def listen_keyboard(button_object)
-    puts 'Inicializando Botão'
+    puts "Escutando Botão #{button_object.pin}"
     thread = Thread.new do
       loop do
         button_object.wait_for_change
-          if button_object.value == 1
-            puts "HIGH :D #{button_object.pin}"
+        if button_object.read == 1
+          puts "HIGH :D #{button_object.pin}"
+          sleep(1)
+          if button_object.read == 1
+            puts "Still HIGH :D #{button_object.pin}"
           end
+        end
       end
     end
     thread.abort_on_exception = true
@@ -31,6 +37,7 @@ class Keyboard
   def wait_keyboard
     listen_keyboard(@button1)
     listen_keyboard(@button2)
+    listen_keyboard(@button3)
     wait
   end
 
